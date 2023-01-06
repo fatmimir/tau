56b6a761785d4fbfa80cb28279ca900e81ffa4bf
Fat mimir <fatmimir@bitid.studio>
2023-01-06 17:22:37 -0600

    parser: Parse bit or and xor expressions

--- tau-build/src/parser_internal.c
+++ tau-build/src/parser_internal.c
@@ -50,7 +50,7 @@ struct tau_node *node_new_binary(enum tau_node_type type, struct tau_token token
 // NOLINTNEXTLINE(misc-no-recursion)
 struct tau_node *parse_expr(struct tau_token *ahead) {
   assert(ahead != NULL && "parse_expr: ahead cannot be NULL");
-  return parse_bit_and_expr(ahead);
+  return parse_bit_or_expr(ahead);
 }
 
 struct tau_node *parse_cast_expr(struct tau_token *ahead) {
@@ -73,8 +73,34 @@ struct tau_node *parse_rel_expr(struct tau_token *ahead) {
   return NULL;
 }
 
+// NOLINTNEXTLINE(misc-no-recursion)
 struct tau_node *parse_bit_or_expr(struct tau_token *ahead) {
   assert(ahead != NULL && "parse_bit_or_expr: ahead cannot be NULL");
+  struct tau_node *left = parse_bit_and_expr(ahead);
+  for (;;) {
+    struct tau_token infix_token = *ahead;
+    if (match_and_consume(ahead, TAU_TOKEN_TYPE_PUNCT, TAU_PUNCT_PIPE, TAU_KEYWORD_NONE)) {
+      struct tau_node *right = parse_bit_and_expr(ahead);
+      MUST_OR_FAIL(right, ahead, "<expression>");
+      left = node_new_binary(TAU_NODE_BIT_OR_EXPR, infix_token, left, right);
+      continue;
+    }
+
+    if (match_and_consume(ahead, TAU_TOKEN_TYPE_PUNCT, TAU_PUNCT_CIRC, TAU_KEYWORD_NONE)) {
+      struct tau_node *right = parse_bit_and_expr(ahead);
+      MUST_OR_FAIL(right, ahead, "<expression>");
+      left = node_new_binary(TAU_NODE_BIT_XOR_EXPR, infix_token, left, right);
+      continue;
+    }
+
+    break;
+  }
+
+  return left;
+handle_fail:
+  if (left != NULL) {
+    node_free(left);
+  }
   return NULL;
 }
 
--- tau-build/test/parser_test.c
+++ tau-build/test/parser_test.c
@@ -340,6 +340,33 @@ static void test_parse_bit_and_expr(void **state) {
   assert_true(match_and_consume(&token, TAU_TOKEN_TYPE_EOL, TAU_PUNCT_NONE, TAU_KEYWORD_NONE));
 }
 
+static void test_parse_bit_or_expr(void **state) {
+  UNUSED(state);
+  const char *test = "a|b; a|b^c; a|b&c;";
+  struct tau_token start = tau_token_start(__func__, test, strlen(test));
+  struct tau_token token = tau_token_next(start);
+  struct tau_node *node = NULL;
+
+  node = parse_bit_or_expr(&token);
+  assert_non_null(node);
+  assert_node_topology(node, "(BIT_OR_EXPR a b)");
+  node_free(node);
+  assert_true(match_and_consume(&token, TAU_TOKEN_TYPE_EOL, TAU_PUNCT_NONE, TAU_KEYWORD_NONE));
+
+  node = parse_bit_or_expr(&token);
+  assert_non_null(node);
+  assert_node_topology(node, "(BIT_XOR_EXPR (BIT_OR_EXPR a b) c)");
+  node_free(node);
+  assert_true(match_and_consume(&token, TAU_TOKEN_TYPE_EOL, TAU_PUNCT_NONE, TAU_KEYWORD_NONE));
+
+  node = parse_bit_or_expr(&token);
+  assert_non_null(node);
+  assert_node_topology(node, "(BIT_OR_EXPR a (BIT_AND_EXPR b c))");
+  node_free(node);
+  assert_true(match_and_consume(&token, TAU_TOKEN_TYPE_EOL, TAU_PUNCT_NONE, TAU_KEYWORD_NONE));
+}
+
+
 int main() {
   UNUSED_TYPE(jmp_buf);
   UNUSED_TYPE(va_list);
@@ -358,6 +385,7 @@ int main() {
       cmocka_unit_test(test_parse_term_expr),             // <expr> <+ -> <expr>,
       cmocka_unit_test(test_parse_bit_shift_expr),        // <expr> <'>>' '<<'> <expr>,
       cmocka_unit_test(test_parse_bit_and_expr),          // <expr> & <expr>,
+      cmocka_unit_test(test_parse_bit_or_expr),           // <expr> <| ^> <expr>,
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);
